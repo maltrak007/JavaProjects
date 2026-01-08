0f9977e83ba115f79170863fda5753698bdc1a93
DrMalboro07 <gonza7108@gmail.com>
2026-01-08 15:12:23 +0100

    Clase Orden-Venta
    Pequeño ejercicio de un sistema de ventas

--- JavaRememberExercise/src/main/java/PruebaPersona/PruebaPersona.java
+++ JavaRememberExercise/src/main/java/PruebaPersona/PruebaPersona.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.gonzalolinanaldana.javarememberexercise;
+package PruebaPersona;
 
 import java.util.Scanner;
 
@@ -10,31 +10,14 @@ import java.util.Scanner;
  *
  * @author maltr
  */
-public class Persona {
-    
-    String nombre;
-    String apellido;
-    int edad;
-    
-    public Persona()
-    {
-        
-    }
-    
-    public Persona(String _nombre, String _apellido, int _edad) {
-        this.nombre = _nombre;
-        this.apellido = _apellido;
-        this.edad = _edad;
-    }
-        
+public class PruebaPersona {
     public static void main(String[] args) {
         Persona[] grupoPersonas;
         Scanner scanner = new Scanner(System.in);
         IO.println("***Crea Personas*** \n ¿Cuantas personas quieres crear?");
         int numCreaciones = Integer.parseInt(scanner.nextLine());
         grupoPersonas = new Persona[numCreaciones];
-        for(int i=0; i < numCreaciones; i++)
-        {
+        for (int i = 0; i < numCreaciones; i++) {
             IO.println("\n Persona numero = " + i);
             IO.println("\nNombre:\t");
             String nombrePersona = scanner.nextLine();
@@ -42,14 +25,12 @@ public class Persona {
             String apellidoPersona = scanner.nextLine();
             IO.println("\n Edad: \t");
             int edadPersona = Integer.parseInt(scanner.nextLine());
-            
+
             grupoPersonas[i] = new Persona(nombrePersona, apellidoPersona, edadPersona);
         }
-        for(Persona persona : grupoPersonas)
-        {
-            String datosFormateados = String.format("Nombre: %s" + " Apellido: %s" + " Edad: %d",persona.nombre,persona.apellido,persona.edad);
+        for (Persona persona : grupoPersonas) {
+            String datosFormateados = String.format("Nombre: %s" + " Apellido: %s" + " Edad: %d", persona.getNombre(), persona.getApellido(), persona.getEdad());
             IO.println(datosFormateados);
         }
     }
 }
-
