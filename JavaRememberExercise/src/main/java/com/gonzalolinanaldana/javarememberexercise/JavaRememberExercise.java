a40431e033a961244accf1014d93f30fbb02851e
DrMalboro07 <gonza7108@gmail.com>
2026-01-08 09:50:36 +0100

    JavaProjects first commit

---
+++ JavaRememberExercise/src/main/java/com/gonzalolinanaldana/javarememberexercise/JavaRememberExercise.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gonzalolinanaldana.javarememberexercise;
+
+/**
+ *
+ * @author maltr
+ */
+public class JavaRememberExercise {
+
+    public static void main(String[] args) {
+       String nombreUsuario;
+       String nombreUsuarioNormalizado;
+       String nombreEmpresa;
+       String extensionDominio;
+       String emailNormalizado;
+       String emailFinal;
+       
+       nombreUsuario = "Ubaldo Acosta Soto";
+       nombreUsuarioNormalizado = nombreUsuario.toLowerCase().strip().replace(" ", ".");
+       nombreEmpresa = "  Global Mentoring";
+       extensionDominio = ".com.mx";
+       emailNormalizado = "@" + nombreEmpresa.trim().replace(" ", ".").toLowerCase() + extensionDominio;
+       IO.println("Email final generado: " + nombreUsuarioNormalizado + emailNormalizado);
+    }
+}
